a610e5fee05b74061276e9c3d89907bd2f2a6280
shiyutian <shiyutian@definesys.com>
2019-11-05 15:02:00 +0800

    修改相关插件的要替换字段

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,4 +1,12 @@
-const plugin = require('./sdmp');
+/*
+ * @Author: Devin Shi
+ * @Email: yutian.shi@definesys.com
+ * @Date: 2019-11-05 11:42:51
+ * @LastEditTime: 2019-11-05 14:46:15
+ * @LastEditors: Devin Shi
+ * @Description:
+ */
+const plugin = require('./$${PLUGIN_NAME}$$');
 
 const PLUGIN_INDEX = plugin.PluginsIndex;
 
